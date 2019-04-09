5f60d7c08cc45c2039302710588ec20f79ac0771
Unknown <yugi.man22@gmail.com>
2019-04-09 23:22:11 +0100

    Novos botões
    Ao clicar no menu os botões fodem-se

--- igo/src/components/Ar.js
+++ igo/src/components/Ar.js
@@ -33,7 +33,10 @@ export default class Ar extends Component {
   render() {
     return (
       <div>
-        <img style={{ zoom: "100%", overflowX: "visible" }} src={this.state.imgArray[this.state.currentImg]} alt=""></img>
+        <Image style={{ zoom: "50%", overflowX: "visible" }} src={this.state.imgArray[this.state.currentImg]} alt=""></Image>
+        <PreviousButton onClick={this.changeImg}>
+          <i class="fas fa-arrow-left"></i>
+        </PreviousButton>
         <NextButton onClick={this.changeImg}>
           <i className="fas fa-arrow-right"></i>
         </NextButton>
@@ -42,11 +45,28 @@ export default class Ar extends Component {
   }
 }
 
-const NextButton = styled.button`
-  background: transparent;
+const PreviousButton = styled.button`
+  position: absolute;
+  background: white;
   border: 1px solid black;
   border-radius: 10px;
   height: 20px;
   width: 50px;
   margin-left: 5px;
+  margin-top: 165px;
+`
+
+const NextButton = styled.button`
+  position: absolute;
+  background: white;
+  border: 1px solid black;
+  border-radius: 10px;
+  height: 20px;
+  width: 50px;
+  margin-left: 95px;
+  margin-top: 165px;
+`
+
+const Image = styled.img`
+  margin-top: -19px;
 `
