4f98e09bc72f9412db5f27126fa74053695f8140
Unknown <yugi.man22@gmail.com>
2019-04-09 01:02:50 +0100

    ImgData for Ar

---
+++ igo/src/ArData.js
@@ -0,0 +1,3 @@
+const ArData = ["img/Mosteiro.svg", "img/Frame2.svg"]
+
+export default ArData
--- igo/src/components/Ar.js
+++ igo/src/components/Ar.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react'
-import Jero from "../Frame2.svg"
+import ArData from "../ArData"
 import Draggable from 'react-draggable';
+import styled from "styled-components"
 
 export default class Ar extends Component {
+  constructor() {
+    super();
+    this.state = {
+        imgArray: ArData,
+        currentImg: 0
+    }
+    this.changeImg = this.changeImg.bind(this)
+  }
+
+  changeImg() {
+    this.setState(prevState => {
+      let currentIndex = prevState.currentImg;
+
+      if (currentIndex === prevState.imgArray.length - 1) {
+        currentIndex = 0
+      }
+      else {
+        currentIndex++
+      }
+      return {
+        imgArray: prevState.imgArray,
+        currentImg: currentIndex
+      }
+    })
+  }
+
   render() {
     return (
-        <img style={{zoom: "100%", overflowX: "visible"}} src={Jero} alt=""></img>
+      <div>
+        <img style={{zoom: "100%", overflowX: "visible"}} src={this.state.imgArray[this.state.currentImg]} alt=""></img>
+        <button onClick={this.changeImg}>
+          <i className="fas fa-arrow-right"></i>
+        </button>
+      </div>
     )
   }
 }
