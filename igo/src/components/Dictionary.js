952eb901c8bc5cf6420ab65386e974fa241ec824
francisco guedes <guedes.silva.francisco@gmail.com>
2019-04-28 15:35:29 +0100

    new codes + dinamic languages

--- igo/src/components/Dictionary.js
+++ igo/src/components/Dictionary.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react'
-import styled from "styled-components" 
+import React, { Component } from "react";
+import styled from "styled-components";
+import codes from "./../lang/codes.json";
 
 export default class Dictionary extends Component {
   constructor(props) {
     super(props);
     this.state = {
       value: "Search a word",
-      invalidInput: false
+      invalidInput: false,
+      lang: "0"
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -21,8 +23,7 @@ export default class Dictionary extends Component {
         value: event.target.value,
         invalidInput: false
       });
-    }
-    else {
+    } else {
       this.setState({
         value: event.target.value,
         invalidInput: true
@@ -56,83 +57,94 @@ export default class Dictionary extends Component {
     return (
       <DictionaryWrapper>
         <div className="main-header">
-            <MainHeader>Dictionary</MainHeader>
+          <MainHeader>Dictionary</MainHeader>
         </div>
         <div className="language-input">
-            <LanguageInput>based on your current location <i class="fas fa-sort-down"></i></LanguageInput>
+          <LanguageInput>
+            {codes[this.state.lang]} <i class="fas fa-sort-down" />
+          </LanguageInput>
         </div>
         <div className="word-input">
           <Box invalidInput={this.state.invalidInput}>
             <form onSubmit={this.handleSubmit}>
-              <TextInput type="text" value={this.state.value} onChange={this.handleChange} onFocus={this.onFocus} onBlur={this.onBlur} />
-              <SearchButton type="submit" value="Submit"><i class="fas fa-search"></i></SearchButton>
+              <TextInput
+                type="text"
+                value={this.state.value}
+                onChange={this.handleChange}
+                onFocus={this.onFocus}
+                onBlur={this.onBlur}
+              />
+              <SearchButton type="submit" value="Submit">
+                <i class="fas fa-search" />
+              </SearchButton>
             </form>
           </Box>
-          <TextAlert invalidInput={this.state.invalidInput}>Vai dar merda!</TextAlert>
+          <TextAlert invalidInput={this.state.invalidInput}>
+            Change input
+          </TextAlert>
         </div>
         <div className="line-left">
-          <Line></Line>
+          <Line />
         </div>
         <div className="side-header">
           <SideHeader>Top Words</SideHeader>
         </div>
         <div className="line-right">
-          <Line></Line>
+          <Line />
         </div>
       </DictionaryWrapper>
-    )
+    );
   }
 }
 
 const DictionaryWrapper = styled.div`
-    margin-top: -10px;
-    display: grid;
-    grid-gap: 3px;
-    grid-template-columns: 45px 1fr 45px;
-    grid-template-rows: 30px 50px 70px 50px;
-    grid-template-areas: 
-        ". main-header ."
-        "language-input language-input language-input"
-        "word-input word-input word-input"
-        "line-left side-header line-right";
-`
+  margin-top: -10px;
+  display: grid;
+  grid-gap: 3px;
+  grid-template-columns: 45px 1fr 45px;
+  grid-template-rows: 30px 50px 70px 50px;
+  grid-template-areas:
+    ". main-header ."
+    "language-input language-input language-input"
+    "word-input word-input word-input"
+    "line-left side-header line-right";
+`;
 
 const MainHeader = styled.p`
   text-align: center;
   font-size: 20px;
   font-weight: bold;
-`
+`;
 
 const LanguageInput = styled.p`
   text-align: center;
   font-size: 12px;
-`
+`;
 
 const SideHeader = styled.p`
   text-align: center;
   font-size: 18px;
   font-weight: bold;
-`
+`;
 
 const Line = styled.hr`
   height: 1px;
   background-color: black;
   margin-top: 28px;
-`
+`;
 
 const Box = styled.div`
-  border: 1px solid ${props => props.invalidInput ? 
-  "var(--mainRed)" : "var(--mainBlack)"};
+  border: 1px solid
+    ${props => (props.invalidInput ? "var(--mainRed)" : "var(--mainBlack)")};
   border-radius: 10px;
   height: 22px;
   width: 200px;
   margin-left: 15px;
 
   .fa-search {
-    color: ${props => props.invalidInput ? 
-    "grey" : "black"};
+    color: ${props => (props.invalidInput ? "grey" : "black")};
   }
-`
+`;
 
 const TextInput = styled.input`
   font-family: "Montserrat";
@@ -144,18 +156,17 @@ const TextInput = styled.input`
   &:focus {
     outline: none;
   }
-`
+`;
 
 const SearchButton = styled.button`
   background-color: transparent;
   border: none;
   margin-left: 10px;
-`
+`;
 
 const TextAlert = styled.p`
-  display: ${props => props.invalidInput ? 
-  "block" : "none"};
+  display: ${props => (props.invalidInput ? "block" : "none")};
   font-size: 14px;
   text-align: left;
   margin-left: 18px;
-`
+`;
--- igo/src/components/Translator.js
+++ igo/src/components/Translator.js
@@ -1,36 +1,68 @@
-import React, { Component } from 'react'
-import styled from "styled-components" 
+import React, { Component } from "react";
+import styled from "styled-components";
+import codes from "./../lang/codes.json";
 
 export default class Translator extends Component {
-    render() {
-        return (
-            <TranslatorWrapper>
-                <div className="main-tr-header">
-                    <MainHeader>Translator</MainHeader>
-                </div>
-                <div className="in-language-input">
-                    <LanguageInput>Portuguese &nbsp; <i class="fas fa-arrow-circle-right"></i> &nbsp; English</LanguageInput>
-                </div>
-{/*                 <ExtraButtons className="extra-buttons">
+  constructor(props) {
+    super(props);
+    this.state = {
+      fromLang: "pt",
+      toLang: "en"
+    };
+
+    this.changeFromLang = this.changeFromLang.bind(this);
+    this.changeToLang = this.changeToLang.bind(this);
+  }
+
+  changeFromLang(newLang) {
+    this.setState({
+      fromLang: newLang,
+      toLang: this.state.toLang
+    });
+  }
+
+  changeToLang(newLang) {
+    this.setState({
+      fromLang: this.state.fromLang,
+      toLang: newLang
+    });
+  }
+
+  render() {
+    return (
+      <TranslatorWrapper>
+        <div className="main-tr-header">
+          <MainHeader>Translator</MainHeader>
+        </div>
+        <div className="in-language-input">
+          <LanguageInput>
+            {codes[this.state.fromLang]} &nbsp;{" "}
+            <i class="fas fa-arrow-circle-right" /> &nbsp;
+            {codes[this.state.toLang]}
+          </LanguageInput>
+        </div>
+        {/*                 <ExtraButtons className="extra-buttons">
                     <i class="fas fa-camera"></i> &nbsp;
                     <i class="fas fa-microphone"></i>
                 </ExtraButtons> */}
-                <div className="word-tr-input">
-                    <Box>
-                        <form>
-                            <TextInput type="text" />
-                            <SearchButton type="submit" value="Submit">Translate</SearchButton>
-                        </form>
-                        <Line></Line>
-                        <form>
-                            <TextInput type="text" />
-                        </form>
-                    </Box>
-                </div>
-{/*                 <div className="out-language-input">
+        <div className="word-tr-input">
+          <Box>
+            <form>
+              <TextInput type="text" />
+              <SearchButton type="submit" value="Submit">
+                Translate
+              </SearchButton>
+            </form>
+            <Line />
+            <form>
+              <TextInput type="text" />
+            </form>
+          </Box>
+        </div>
+        {/*                 <div className="out-language-input">
                     <LanguageInput>English <i class="fas fa-sort-down"></i></LanguageInput>
                 </div> */}
-{/*                 <div className="word-tr-output">
+        {/*                 <div className="word-tr-output">
                     <Box>
                         <form>
                             <TextInput type="text" />
@@ -38,47 +70,47 @@ export default class Translator extends Component {
                         </form>
                     </Box>
                 </div> */}
-            </TranslatorWrapper>
-        )
-    }
+      </TranslatorWrapper>
+    );
+  }
 }
 
 const TranslatorWrapper = styled.div`
-    margin-top: -10px;
-    display: grid;
-    grid-gap: 5px;
-    grid-template-columns: 10px 45px 1fr 45px;
-    grid-template-rows: 30px 10px 30px 5px 100px;
-    grid-template-areas:
-        "main-tr-header main-tr-header main-tr-header main-tr-header" 
-        ". . . ."
-        ". in-language-input in-language-input in-language-input"
-        ". . . ."
-        ". word-tr-input word-tr-input word-tr-input";
-`
+  margin-top: -10px;
+  display: grid;
+  grid-gap: 5px;
+  grid-template-columns: 10px 45px 1fr 45px;
+  grid-template-rows: 30px 10px 30px 5px 100px;
+  grid-template-areas:
+    "main-tr-header main-tr-header main-tr-header main-tr-header"
+    ". . . ."
+    ". in-language-input in-language-input in-language-input"
+    ". . . ."
+    ". word-tr-input word-tr-input word-tr-input";
+`;
 
 const MainHeader = styled.p`
   text-align: center;
   font-size: 20px;
   font-weight: 700;
-`
+`;
 
 const Line = styled.hr`
   height: 1px;
   background-color: grey;
   margin-top: -0.5px;
-`
+`;
 
 const LanguageInput = styled.p`
   text-align: left;
   font-size: 14px;
   font-weight: 500;
-`
+`;
 
 const ExtraButtons = styled.div`
-    margin-top: 15px;
-    font-size: 14px;
-`
+  margin-top: 15px;
+  font-size: 14px;
+`;
 
 const TextInput = styled.input`
   font-family: "Montserrat";
@@ -89,7 +121,7 @@ const TextInput = styled.input`
   &:focus {
     outline: none;
   }
-`
+`;
 
 const SearchButton = styled.button`
   font-family: "Montserrat";
@@ -102,7 +134,7 @@ const SearchButton = styled.button`
   left: 40px;
   width: 115px;
   height: 27px;
-`
+`;
 
 const Box = styled.div`
   border-radius: 5px;
@@ -113,4 +145,4 @@ const Box = styled.div`
   .fa-search {
     color: black;
   }
-`
+`;
