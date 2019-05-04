47469ef7a83cac796c704993c33f1c17b12fa4f3
Unknown <ricardo.s.fernandes@tecnico.ulisboa.pt>
2019-05-04 11:30:27 +0100

    menu sidebar white overlay

--- igo/src/index.js
+++ igo/src/index.js
@@ -8,6 +8,28 @@ import * as serviceWorker from "./serviceWorker";
 ReactDOM.render(
     <Router>
         <App />
+        <div
+            style={{
+                backgroundColor: "white",
+                position: "absolute",
+                left: "calc(50% - 325px)",
+                zIndex: "1201",
+                top: "0px",
+                width: "200px",
+                height: "400px"
+            }}
+        />
+        <div
+            style={{
+                backgroundColor: "black",
+                position: "absolute",
+                left: "calc(50% - 325px)",
+                zIndex: "1200",
+                top: "25px",
+                width: "209.5px",
+                height: "360px"
+            }}
+        />
     </Router>,
     document.getElementById("root")
 );
