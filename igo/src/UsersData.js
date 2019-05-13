2955b27363cae803f7f82b3ed76fe72ceff13868
francisco guedes <guedes.silva.francisco@gmail.com>
2019-05-14 00:17:03 +0100

    pre-user name filter

--- igo/src/UsersData.js
+++ igo/src/UsersData.js
@@ -1,45 +1,44 @@
 const UsersData = [
     {
-      id: 1,
-      userId: 1,
-      avatar: "img/Zequinha.png",
-      name: "Artur Dias",
-      description: "Vizinho do 6º C",
-      added: true
+        id: 1,
+        userId: 1,
+        avatar: "img/Zequinha.png",
+        name: "Artur Dias",
+        description: "Vizinho do 6º C",
+        added: true
     },
     {
-      id: 2,
-      userId: 2,
-      avatar: "img/Sandra.png",
-      name: "Maria dos Santos",
-      description: "BFF 🥰🥰",
-      added: true
+        id: 2,
+        userId: 2,
+        avatar: "img/Sandra.png",
+        name: "Maria dos Santos",
+        description: "BFF 🥰🥰",
+        added: true
     },
     {
-      id: 3,
-      userId: 3,
-      avatar: "img/Susana.png",
-      name: "Susana Sá",
-      description: "Colega de Bioquímica",
-      added: true
+        id: 3,
+        userId: 3,
+        avatar: "img/Susana.png",
+        name: "Susana Sá",
+        description: "Colega de Bioquímica",
+        added: true
     },
     {
-      id: 4,
-      userId: 4,
-      avatar: "img/Artur.png",
-      name: "Zequinha",
-      description: "Avô",
-      added: true
+        id: 4,
+        userId: 4,
+        avatar: "img/Artur.png",
+        name: "Zequinha",
+        description: "Avô",
+        added: true
     },
     {
-      id: 5,
-      userId: 5,
-      avatar: "img/Artur.png",
-      name: "Zequinha",
-      description: "Avô 2",
-      added: false
+        id: 5,
+        userId: 5,
+        avatar: "img/Artur.png",
+        name: "Zequinha",
+        description: "",
+        added: false
     }
-  ];
-  
-  export default UsersData;
-  
+];
+
+export default UsersData;
--- igo/src/components/UsersList.js
+++ igo/src/components/UsersList.js
@@ -1,7 +1,7 @@
-import React, { useContext } from 'react'
-import { AppContext } from "../context"
-import User from "./User"
-import styled from "styled-components"
+import React, { useContext } from "react";
+import { AppContext } from "../context";
+import User from "./User";
+import styled from "styled-components";
 
 export default function UsersLists() {
     const value = useContext(AppContext);
@@ -9,9 +9,20 @@ export default function UsersLists() {
     return (
         <>
             <UsersHeader>All Users</UsersHeader>
+            <div className="word-input">
+                <Box>
+                    <form>
+                        <TextInput type="text" placeholder="Find an user" />
+                        <SearchButton type="submit" value="Submit">
+                            <i className="fas fa-search" />
+                        </SearchButton>
+                    </form>
+                </Box>
+                <TextAlert>Invalid input</TextAlert>
+            </div>
             {value.state.usersData.map(item => {
-                if (!item.added) {
-                    return <User key={item.id} item={item} />   
+                if (!item.added && item.name.includes(KEYBOARDINPUT AQUI)) {
+                    return <User key={item.id} item={item} />;
                 }
             })}
         </>
@@ -21,4 +32,46 @@ export default function UsersLists() {
 const UsersHeader = styled.h3`
     text-align: center;
     margin-bottom: 10px;
-`
+`;
+
+const TextAlert = styled.p`
+    display: ${props => (props.invalidInput ? "block" : "none")};
+    font-size: 14px;
+    text-align: left;
+    margin-left: 18px;
+`;
+
+const TextInput = styled.input`
+    font-family: "Montserrat";
+    background-color: transparent;
+    width: 150px;
+    margin-left: 10px;
+    border: none;
+
+    &:focus {
+        outline: none;
+    }
+`;
+
+const SearchButton = styled.button`
+    background-color: transparent;
+    border: none;
+    margin-left: 10px;
+`;
+
+const Box = styled.div`
+    border: ${props =>
+        props.invalidInput
+            ? "1px solid var(--mainRed)"
+            : props.success
+            ? "1px solid var(--mainGreen)"
+            : "1px solid var(--mainBlack)"};
+    border-radius: 10px;
+    height: 22px;
+    width: 200px;
+    margin-left: 15px;
+
+    .fa-search {
+        color: ${props => (props.invalidInput ? "grey" : "black")};
+    }
+`;
