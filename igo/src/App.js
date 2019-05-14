f6287168f1ddc7f33f202258ba6db95e8b9f6971
Unknown <ricardo.s.fernandes@tecnico.ulisboa.pt>
2019-05-14 10:51:41 +0100

    merge?

--- igo/src/App.js
+++ igo/src/App.js
@@ -24,212 +24,212 @@ var history;
 var backAction;
 
 class App extends Component {
-    constructor() {
-        super();
-        history = createBrowserHistory();
-        backAction = null;
-    }
+	constructor() {
+		super();
+		history = createBrowserHistory();
+		backAction = null;
+	}
 
-    state = {
-        keyboard: false,
-        keyboardInput: "",
-        infoPopup: null
-    };
+	state = {
+		keyboard: false,
+		keyboardInput: "",
+		infoPopup: null
+	};
 
-    styles = {
-        keyboardOn: {
-            width: "auto",
-            height: "auto",
-            position: "absolute",
-            zIndex: "11",
-            bottom: -5,
-            left: -9,
-            transform: "scale(0.915)",
-            backgroundColor: "#eeeeee"
-        },
-        keyboardOff: {
-            width: "auto",
-            height: "auto",
-            transform: "scale(0)"
-        }
-    };
+	styles = {
+		keyboardOn: {
+			width: "auto",
+			height: "auto",
+			position: "absolute",
+			zIndex: "11",
+			bottom: -5,
+			left: -9,
+			transform: "scale(0.915)",
+			backgroundColor: "#eeeeee"
+		},
+		keyboardOff: {
+			width: "auto",
+			height: "auto",
+			transform: "scale(0)"
+		}
+	};
 
-    handleBack() {
-        if (backAction === null) {
-            this.setInput("");
-            history.goBack();
-        } else {
-            backAction();
-            backAction = null;
-        }
-    }
+	handleBack() {
+		if (backAction === null) {
+			this.setInput("");
+			history.goBack();
+		} else {
+			backAction();
+			backAction = null;
+		}
+	}
 
-    handleHome() {
-        this.toggleKeyboard(false);
-        this.setInput("");
-        history.push("/");
-        history.push("/");
-        history.goBack();
-    }
+	handleHome() {
+		this.toggleKeyboard(false);
+		this.setInput("");
+		history.push("/");
+		history.push("/");
+		history.goBack();
+	}
 
-    toggleKeyboard(state) {
-        if (state === undefined) state = !this.state.keyboard;
-        if (state !== this.state.keyboard) {
-            if (!this.state.keyboard) {
-                backAction = () => this.toggleKeyboard(false);
-                this.setState({ keyboard: true });
-            } else {
-                backAction = null;
-                this.setState({ keyboard: false });
-            }
-        }
-    }
+	toggleKeyboard(state) {
+		if (state === undefined) state = !this.state.keyboard;
+		if (state !== this.state.keyboard) {
+			if (!this.state.keyboard) {
+				backAction = () => this.toggleKeyboard(false);
+				this.setState({ keyboard: true });
+			} else {
+				backAction = null;
+				this.setState({ keyboard: false });
+			}
+		}
+	}
 
-    getKeyboardStyle() {
-        return this.state.keyboard
-            ? this.styles.keyboardOn
-            : this.styles.keyboardOff;
-    }
+	getKeyboardStyle() {
+		return this.state.keyboard
+			? this.styles.keyboardOn
+			: this.styles.keyboardOff;
+	}
 
-    setInput(input) {
-        this.setState({ keyboardInput: input });
-    }
+	setInput(input) {
+		this.setState({ keyboardInput: input });
+	}
 
-    render() {
-        return (
-            <AppProvider>
-                <div className="band">
-                    <div id="App">
-                        {this.state.infoPopup}
-                        <Sidebar
-                            disableAutoFocus
-                            pageWrapId={"page-wrap"}
-                            outerContainerId={"App"}
-                            keyboardToggle={state => this.toggleKeyboard(state)}
-                        />
-                        <div className="content" id="page-wrap">
-                            <Navbar history={history} />
-                            <div id="page-component">
-                                <Switch>
-                                    <Route
-                                        exact={true}
-                                        path="/ar"
-                                        component={Ar}
-                                    />
-                                    <Route
-                                        exact={true}
-                                        path="/dictionary"
-                                        render={props => (
-                                            <Dictionary
-                                                keyboardInput={
-                                                    this.state.keyboardInput
-                                                }
-                                                keyboardToggle={state =>
-                                                    this.toggleKeyboard(state)
-                                                }
-                                                {...props}
-                                            />
-                                        )}
-                                    />
-                                    <Route
-                                        exact={true}
-                                        path="/translator"
-                                        render={props => (
-                                            <Translator
-                                                keyboardInput={
-                                                    this.state.keyboardInput
-                                                }
-                                                keyboardToggle={state =>
-                                                    this.toggleKeyboard(state)
-                                                }
-                                                {...props}
-                                            />
-                                        )}
-                                    />
-                                    <Route
-                                        exact={true}
-                                        path="/translatorar"
-                                        component={TranslatorAr}
-                                    />
-                                    <Route
-                                        exact={true}
-                                        path="/sharoute"
-                                        component={Sharoute}
-                                    />
-                                    <Route
-                                        exact={true}
-                                        path="/popularroutes"
-                                        component={PopularRoutes}
-                                    />
-                                    <Route
-                                        exact={true}
-                                        path="/location"
-                                        component={LocationList}
-                                    />
-                                    <Route
-                                        exact={true}
-                                        path="/friends"
-                                        component={FriendsList}
-                                    />
-                                    <Route
-                                        exact={true}
-                                        path="/users"
-                                        render={props => (
-                                            <UsersList
-                                                keyboardInput={
-                                                    this.state.keyboardInput
-                                                }
-                                                keyboardToggle={state =>
-                                                    this.toggleKeyboard(state)
-                                                }
-                                                {...props}
-                                            />
-                                        )}
-                                    />
-                                    <Route
-                                        exact={true}
-                                        path="/"
-                                        component={Feed}
-                                    />
-                                    <Route
-                                        path="/:handle"
-                                        component={Profile}
-                                    />
-                                </Switch>
-                            </div>
-                        </div>
-                        <div style={this.getKeyboardStyle()}>
-                            <Keyboard
-                                onChange={input => this.setInput(input)}
-                                layout={{
-                                    default: [
-                                        "Q W E R T Y U I O P {bksp}",
-                                        "{whitesp} A S D F G H J K L {whitesp}",
-                                        "{whitesp} {whitesp} Z X C V B N M {whitesp} {whitesp}",
-                                        "{space}"
-                                    ]
-                                }}
-                                display={{
-                                    "{bksp}": "⬅",
-                                    "{enter}": "↵",
-                                    "{whitesp}": " ",
-                                    "{space}": "______"
-                                }}
-                            />
-                        </div>
-                    </div>
-                    <ButtonHome
-                        onClick={() => this.handleHome()}
-                        history={history}
-                    />
-                    <ButtonBack
-                        onClick={() => this.handleBack()}
-                        history={history}
-                    />
-                </div>
-            </AppProvider>
-        );
-    }
+	render() {
+		return (
+			<AppProvider>
+				<div className="band">
+					<div id="App">
+						{this.state.infoPopup}
+						<Sidebar
+							disableAutoFocus
+							pageWrapId={"page-wrap"}
+							outerContainerId={"App"}
+							keyboardToggle={state => this.toggleKeyboard(state)}
+						/>
+						<div className="content" id="page-wrap">
+							<Navbar history={history} />
+							<div id="page-component">
+								<Switch>
+									<Route
+										exact={true}
+										path="/ar"
+										component={Ar}
+									/>
+									<Route
+										exact={true}
+										path="/dictionary"
+										render={props => (
+											<Dictionary
+												keyboardInput={
+													this.state.keyboardInput
+												}
+												keyboardToggle={state =>
+													this.toggleKeyboard(state)
+												}
+												{...props}
+											/>
+										)}
+									/>
+									<Route
+										exact={true}
+										path="/translator"
+										render={props => (
+											<Translator
+												keyboardInput={
+													this.state.keyboardInput
+												}
+												keyboardToggle={state =>
+													this.toggleKeyboard(state)
+												}
+												{...props}
+											/>
+										)}
+									/>
+									<Route
+										exact={true}
+										path="/translatorar"
+										component={TranslatorAr}
+									/>
+									<Route
+										exact={true}
+										path="/sharoute"
+										component={Sharoute}
+									/>
+									<Route
+										exact={true}
+										path="/popularroutes"
+										component={PopularRoutes}
+									/>
+									<Route
+										exact={true}
+										path="/location"
+										component={LocationList}
+									/>
+									<Route
+										exact={true}
+										path="/friends"
+										component={FriendsList}
+									/>
+									<Route
+										exact={true}
+										path="/users"
+										render={props => (
+											<UsersList
+												keyboardInput={
+													this.state.keyboardInput
+												}
+												keyboardToggle={state =>
+													this.toggleKeyboard(state)
+												}
+												{...props}
+											/>
+										)}
+									/>
+									<Route
+										exact={true}
+										path="/"
+										component={Feed}
+									/>
+									<Route
+										path="/:handle"
+										component={Profile}
+									/>
+								</Switch>
+							</div>
+						</div>
+						<div style={this.getKeyboardStyle()}>
+							<Keyboard
+								onChange={input => this.setInput(input)}
+								layout={{
+									default: [
+										"Q W E R T Y U I O P {bksp}",
+										"{whitesp} A S D F G H J K L {whitesp}",
+										"{whitesp} {whitesp} Z X C V B N M {whitesp} {whitesp}",
+										"{space}"
+									]
+								}}
+								display={{
+									"{bksp}": "⬅",
+									"{enter}": "↵",
+									"{whitesp}": " ",
+									"{space}": "______"
+								}}
+							/>
+						</div>
+					</div>
+					<ButtonHome
+						onClick={() => this.handleHome()}
+						history={history}
+					/>
+					<ButtonBack
+						onClick={() => this.handleBack()}
+						history={history}
+					/>
+				</div>
+			</AppProvider>
+		);
+	}
 }
 //<Button history={history} profile="home" />
 export default App;
--- igo/src/components/UsersList.js
+++ igo/src/components/UsersList.js
@@ -4,88 +4,88 @@ import styled from "styled-components";
 import users from "../UsersData";
 
 export default class UsersLists extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 
-    render() {
-        return (
-            <React.Fragment>
-                <UsersHeader>All Users</UsersHeader>
-                <div className="word-input">
-                    <Box>
-                        <form>
-                            <TextInput
-                                type="text"
-                                value={this.props.keyboardInput}
-                                onClick={this.props.keyboardToggle}
-                                placeholder="Search a word"
-                            />
-                            <SearchButton type="submit" value="Submit">
-                                <i className="fas fa-search" />
-                            </SearchButton>
-                        </form>
-                    </Box>
-                </div>
-                {users.map(item => {
-                    if (
-                        !item.added &&
-                        item.name
-                            .toUpperCase()
-                            .includes(this.props.keyboardInput)
-                    ) {
-                        return <User key={item.id} item={item} />;
-                    }
-                })}
-            </React.Fragment>
-        );
-    }
+	render() {
+		return (
+			<React.Fragment>
+				<UsersHeader>All Users</UsersHeader>
+				<div className="word-input">
+					<Box>
+						<form>
+							<TextInput
+								type="text"
+								value={this.props.keyboardInput}
+								onClick={this.props.keyboardToggle}
+								placeholder="Search a word"
+							/>
+							<SearchButton type="submit" value="Submit">
+								<i className="fas fa-search" />
+							</SearchButton>
+						</form>
+					</Box>
+				</div>
+				{users.map(item => {
+					if (
+						!item.added &&
+						item.name
+							.toUpperCase()
+							.includes(this.props.keyboardInput)
+					)
+						return <User key={item.id} item={item} />;
+				})}
+			</React.Fragment>
+		);
+	}
 }
 
 const UsersHeader = styled.h3`
-    text-align: center;
-    margin-bottom: 10px;
+	text-align: center;
+	margin-bottom: 10px;
 `;
 
-const TextAlert = styled.p`
-    display: ${props => (props.invalidInput ? "block" : "none")};
-    font-size: 14px;
-    text-align: left;
-    margin-left: 18px;
-`;
+/*const TextAlert = styled.p`
+	display: ${props => (props.invalidInput ? "block" : "none")};
+	font-size: 14px;
+	text-align: left;
+	margin-left: 18px;
+`;*/
 
 const TextInput = styled.input`
-    font-family: "Montserrat";
-    background-color: transparent;
-    width: 150px;
-    margin-left: 10px;
-    border: none;
+	font-family: "Montserrat";
+	background-color: transparent;
+	width: 150px;
+	margin-left: 10px;
+	border: none;
 
-    &:focus {
-        outline: none;
-    }
+	&:focus {
+		outline: none;
+	}
 `;
 
 const SearchButton = styled.button`
-    background-color: transparent;
-    border: none;
-    margin-left: 10px;
+	background-color: transparent;
+	border: none;
+	margin-left: 10px;
 `;
 
 const Box = styled.div`
-    border: ${props =>
-        props.invalidInput
-            ? "1px solid var(--mainRed)"
-            : props.success
-            ? "1px solid var(--mainGreen)"
-            : "1px solid var(--mainBlack)"};
-    border-radius: 10px;
-    height: 22px;
-    width: 200px;
-    margin-left: 15px;
+	border: ${props =>
+		props.invalidInput
+			? "1px solid var(--mainRed)"
+			: props.success
+			? "1px solid var(--mainGreen)"
+			: "1px solid var(--mainBlack)"};
+	border-radius: 10px;
+	height: 22px;
+	width: 200px;
+	margin-left: 15px;
+	margin-bottom: 10px;
 
-    .fa-search {
-        color: ${props => (props.invalidInput ? "grey" : "black")};
-    }
+	.fa-search {
+		color: ${props => (props.invalidInput ? "grey" : "black")};
+	}
 `;
