11247628e65813145655cf4988a21a9e4aff6969
Unknown <ricardo.s.fernandes@tecnico.ulisboa.pt>
2019-05-15 02:32:57 +0100

    Profile partially done

--- igo/src/components/Activity.js
+++ igo/src/components/Activity.js
@@ -1,120 +1,158 @@
-import React, { Component } from 'react'
-import { Link } from "react-router-dom"
-import styled from "styled-components"
-import { AppConsumer } from "../context"
-import { Spring } from 'react-spring/renderprops'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { AppConsumer } from "../context";
+import { Spring } from "react-spring/renderprops";
+import Profile from "./Profile";
 
 export default class Activity extends Component {
-  render() {
-    return (
-      <Spring 
-        from={{ opacity: 0, marginTop: -20 }}
-        to={{ opacity: 1, marginTop: 20 }}
-        config={{ delay: 100, duration: 900 }}
-      >
-        {props => <div style={props}>
-            <Link style={{ textDecoration: 'none' }} to={{
-            pathname: this.props.item.name,
-            state: {
-                userId: this.props.item.userId,
-                name: this.props.item.name
-            }
-            }}>
-                <ActivityWrapper>
-                    <Avatar className="avatar">
-                        <img src={this.props.item.avatar} alt="Person"></img>
-                    </Avatar>
-                    <div className="name">
-                        <NameText>{this.props.item.name}</NameText>
-                    </div>
-                    <div className="activity">
-                        <ActivityText>{this.props.item.activity}</ActivityText>
-                    </div>
-                    <div className="timestamp">
-                        <TimeText>{this.props.item.timestamp}</TimeText>
-                    </div>
-                </ActivityWrapper>
-            </Link>
-            <AppConsumer>
-                {value => (
-                    <AddedWrapper>
-                        <div className="comments">
-                            <Added>
-                                <i className="far fa-comment"></i> {this.props.item.comments}
-                            </Added>
-                        </div>
-                        <div className="likes">
-                            <Added>
-                                {this.props.item.liked ? <i onClick={() => value.decreaseLikes(this.props.item.id)} className="fas fa-heart heart-filled"></i> : <i onClick={() => value.increaseLikes(this.props.item.id)} className="far fa-heart"></i>} {this.props.item.likes}
-                            </Added>
-                        </div>
-                    </AddedWrapper>
-                )}
-            </AppConsumer>
-        </div>}
-      </Spring>
-    )
-  }
+	state = {
+		profile: false
+	};
+
+	toggleProfile() {
+		this.setState({ profile: !this.state.profile });
+	}
+
+	render() {
+		return (
+			<Spring
+				from={{ opacity: 0, marginTop: -20 }}
+				to={{ opacity: 1, marginTop: 20 }}
+				config={{ delay: 100, duration: 900 }}
+			>
+				{props => (
+					<div style={props}>
+						<Profile
+							item={this.props.item}
+							status={this.state.profile}
+						/>
+
+						<ActivityWrapper onClick={() => this.toggleProfile()}>
+							<Avatar className="avatar">
+								<img
+									style={{
+										maxWidth: "100%",
+										maxHeight: "100%",
+										borderRadius: "50%"
+									}}
+									src={this.props.item.avatar}
+									alt="Person"
+								/>
+							</Avatar>
+							<div className="name">
+								<NameText>{this.props.item.name}</NameText>
+							</div>
+							<div className="activity">
+								<ActivityText>
+									{this.props.item.activity}
+								</ActivityText>
+							</div>
+							<div className="timestamp">
+								<TimeText>{this.props.item.timestamp}</TimeText>
+							</div>
+						</ActivityWrapper>
+
+						<AppConsumer>
+							{value => (
+								<AddedWrapper>
+									<div className="comments">
+										<Added>
+											<i className="far fa-comment" />{" "}
+											{this.props.item.comments}
+										</Added>
+									</div>
+									<div className="likes">
+										<Added>
+											{this.props.item.liked ? (
+												<i
+													onClick={() =>
+														value.decreaseLikes(
+															this.props.item.id
+														)
+													}
+													className="fas fa-heart heart-filled"
+												/>
+											) : (
+												<i
+													onClick={() =>
+														value.increaseLikes(
+															this.props.item.id
+														)
+													}
+													className="far fa-heart"
+												/>
+											)}{" "}
+											{this.props.item.likes}
+										</Added>
+									</div>
+								</AddedWrapper>
+							)}
+						</AppConsumer>
+					</div>
+				)}
+			</Spring>
+		);
+	}
 }
 
 const ActivityWrapper = styled.div`
-    margin-top: 20px;
-    display: grid;
-    grid-gap: 3px;
-    grid-template-columns: 40px 1fr 1fr;
-    grid-template-rows: 30px 15px 40px;
-    grid-template-areas: 
-        "avatar name name"
-        "avatar timestamp timestamp"
-        "activity activity activity";
-`
+	margin-top: 20px;
+	display: grid;
+	grid-gap: 3px;
+	grid-template-columns: 40px 1fr 1fr;
+	grid-template-rows: 30px 15px 40px;
+	grid-template-areas:
+		"avatar name name"
+		"avatar timestamp timestamp"
+		"activity activity activity";
+`;
 
 const AddedWrapper = styled.div`
-    display: grid;
-    grid-gap: 3px;
-    grid-template-columns: 40px 1fr 1fr;
-    grid-template-rows: 15px;
-    grid-template-areas: 
-        "comments likes .";
-    box-shadow: 2px 3px 2px 1px rgba(0, 0, 0, 0.2);
-`
+	display: grid;
+	grid-gap: 3px;
+	grid-template-columns: 40px 1fr 1fr;
+	grid-template-rows: 15px;
+	grid-template-areas: "comments likes .";
+	box-shadow: 2px 3px 2px 1px rgba(0, 0, 0, 0.2);
+`;
 
 const Avatar = styled.div`
-    display: block;
-    margin-right: auto;
-    margin-left: 10px;
-    margin-top: auto;
-    margin-bottom: auto;
-`
+	display: block;
+	margin-right: auto;
+	margin-left: 10px;
+	margin-top: auto;
+	margin-bottom: auto;
+`;
 
 const NameText = styled.p`
-    font-size: 95%;
-    font-weight: 700;
-    margin-left: 10px;
-    margin-top: 5px;
-    color: black;
-    text-decoration: none;
-`
+	font-size: 95%;
+	font-weight: 700;
+	margin-left: 10px;
+	margin-top: 5px;
+	color: black;
+	text-decoration: none;
+`;
 
 const ActivityText = styled.p`
-    font-size: 80%;
-    font-weight: 500;
-    margin-left: 10px;
-    margin-top: 5px;
-    color: black;
-    text-decoration: none;
-`
+	font-size: 80%;
+	font-weight: 500;
+	margin-left: 10px;
+	margin-top: 5px;
+	color: black;
+	text-decoration: none;
+`;
 
 const TimeText = styled.p`
-    font-size: 70%;
-    margin-left: 10px;
-    margin-top: -9px;
-    color: black;
-    text-decoration: none;
-`
+	font-size: 70%;
+	margin-left: 10px;
+	margin-top: -9px;
+	color: black;
+	text-decoration: none;
+`;
 
 const Added = styled.span`
-    font-size: 12px;
-    margin-left: 12px;
-    top: 10px;
-`
+	font-size: 12px;
+	margin-left: 12px;
+	top: 10px;
+`;
--- igo/src/components/Profile.js
+++ igo/src/components/Profile.js
@@ -1,105 +1,197 @@
-import React, { useContext } from 'react'
-import { AppContext } from "../context"
-import styled from "styled-components"
+import React, { Component } from "react";
+import styled from "styled-components";
+import { AppConsumer } from "../context";
 
-export default function Profile(props) {
-    const value = useContext(AppContext);
-    const profileBg = value.getProfileBg(props.location.state.userId);
-    const status = value.getStatus(props.location.state.userId);
+export default class Profile extends Component {
+	styles = {
+		background: {
+			backgroundColor: "rgba(0, 0, 0, 0.5)",
+			position: "absolute",
+			height: "341px",
+			width: "230px",
+			top: "30px",
+			left: "0px"
+		},
+		avatar: {
+			height: "100px",
+			width: "100px",
+			margin: "auto",
+			marginTop: "5px",
+			marginBottom: "5px",
+			zIndex: 30
+		}
+	};
 
-    return (
-        <ProfileWrapper>
-            <div className="profile-avatar">
-                <img src={profileBg} alt="Person"></img>
-            </div>
-            <div className="profile-name">
-                <NameText>{props.location.state.name}</NameText>
-            </div>
-            <div className="profile-status">
-                <StatusText status={status}>{status ? "Online" : "Offline"} <i class="fas fa-circle"></i></StatusText>
-            </div>
-            <div className="profile-routes">
-                <RoutesText>Routes <i class="fas fa-route"></i></RoutesText>
-                <RoutesCounterText>10</RoutesCounterText>
-            </div>
-            <div className="profile-posts">
-                <PostsText>Posts <i class="far fa-comments"></i></PostsText>
-                <PostsCounterText>10</PostsCounterText>
-            </div>
-            <div className="profile-remove">
-                <RemoveText><i class="fas fa-user-times"></i> Remove Friend</RemoveText>
-            </div>
-        </ProfileWrapper>
-    )
+	getStatus() {
+		if (this.props.status) return { visibility: "visible" };
+		else return { visibility: "hidden" };
+	}
+
+	render() {
+		return (
+			<div style={this.getStatus()}>
+				<div style={this.styles.background}>
+					<ProfileWrapper>
+						<ProfileBanner style={{ backgroundColor: "#FFBFA3" }} />
+						<div
+							className="profile-avatar"
+							style={this.styles.avatar}
+						>
+							<img
+								style={{
+									maxWidth: "96%",
+									maxHeight: "96%",
+									borderRadius: "50%",
+									border: "2px solid black"
+								}}
+								src={this.props.item.avatar}
+								alt="Person"
+							/>
+						</div>
+						<div className="profile-status">
+							<StatusText>
+								Online <i class="fas fa-circle" />
+							</StatusText>
+						</div>
+						<div className="profile-name">
+							<NameText>{this.props.item.name}</NameText>
+						</div>
+						<div className="profile-description">
+							<DescriptionText>
+								{this.props.item.description}
+							</DescriptionText>
+						</div>
+						<div className="profile-routes">
+							<RoutesText>
+								Routes <i class="fas fa-route" />
+							</RoutesText>
+							<RoutesCounterText>10</RoutesCounterText>
+						</div>
+						<div className="profile-posts">
+							<PostsText>
+								Posts <br /> <i class="far fa-comments" />
+							</PostsText>
+							<PostsCounterText>10</PostsCounterText>
+						</div>
+						<div className="profile-remove">
+							<RemoveText>
+								<i class="fas fa-user-times" />
+							</RemoveText>
+						</div>
+						<div className="profile-edit">
+							<EditText>
+								<i class="fas fa-pencil-alt" />
+							</EditText>
+						</div>
+					</ProfileWrapper>
+				</div>
+			</div>
+		);
+	}
 }
 
 const ProfileWrapper = styled.div`
-    margin-top: -10px;
-    display: grid;
-    grid-gap: 3px;
-    grid-template-columns: 5px 1fr 10px 1fr 5px;
-    grid-template-rows: 100px 20px 20px 10px 110px 40px;
-    grid-template-areas: 
-        "profile-avatar profile-avatar profile-avatar profile-avatar profile-avatar"
-        "profile-name profile-name profile-name profile-name profile-name"
-        "profile-status profile-status profile-status profile-status profile-status"
-        ". . . . ."
-        ". profile-routes . profile-posts ."
-        "profile-remove profile-remove profile-remove profile-remove profile-remove";
-`
+	display: grid;
+	grid-gap: 3px;
+	grid-template-columns: 5px 1fr 10px 1fr 5px;
+	grid-template-rows: 119px 20px 15px 27px 65px 20px;
+	grid-template-areas:
+		"profile-avatar profile-avatar profile-avatar profile-avatar profile-avatar"
+		"profile-name profile-name profile-name profile-name profile-name"
+		"profile-description profile-description profile-description profile-description profile-description"
+		"profile-status profile-status profile-status profile-status profile-status"
+		". profile-routes . profile-posts ."
+		". profile-remove . profile-edit .";
+	margin: 20px;
+	background-color: white;
+	padding: 10px;
+	border-radius: 20px;
+	border: 0.5px solid black;
+	overflow: hidden;
+`;
+
+const ProfileBanner = styled.div`
+	position: absolute;
+	height: 59px;
+	width: 188.5px;
+	left: 20px;
+	top: 20px;
+	border-radius: 20px 20px 0px 0px;
+	border: 0.5px solid black;
+`;
 
 const NameText = styled.p`
-    font-size: 100%;
-    font-weight: 700;
-    text-align: center;
-    margin-top: -1px;
-`
+	font-size: 100%;
+	font-weight: 700;
+	text-align: center;
+	margin: auto;
+	margin-top: 0;
+	margin-bottom: 0;
+`;
+
+const DescriptionText = styled.p`
+	font-size: 75%;
+	text-align: center;
+	margin-top: -2px;
+	margin-bottom: 0;
+`;
 
 const RemoveText = styled.p`
-    font-size: 100%;
-    font-weight: 500;
-    text-align: center;
+	text-align: right;
+	margin-top: 0;
+	margin-bottom: 0;
+	font-size: 90%;
+`;
 
-    i {
-        font-size: 90%;
-    }
-`
+const EditText = styled.p`
+	text-align: left;
+	margin-top: 0;
+	margin-bottom: 0;
+	font-size: 90%;
+`;
 
 const StatusText = styled.p`
-    font-size: 80%;
-    margin-top: -1px;
-    text-align: center;
+	font-size: 80%;
+	text-align: center;
 
-    i {
-        position: relative;
-        top: -1px;
-        font-size: 50%;
-        color: ${props => (props.status ? "var(--mainGreen)" : "var(--mainRed)")};
-    }
-`
+	i {
+		position: relative;
+		top: -1px;
+		font-size: 50%;
+		color: var(--mainGreen);
+	}
+	margin-top: 0;
+	margin-bottom: 0;
+`;
 
 const RoutesText = styled.p`
-    font-size: 95%;
-    font-weight: 700;
-    text-align: center;
-`
+	font-size: 95%;
+	font-weight: 700;
+	text-align: center;
+	margin-top: 0px;
+	margin-bottom: 0px;
+`;
 
 const RoutesCounterText = styled.p`
-    font-size: 95%;
-    font-weight: 400;
-    text-align: center;
-    margin-top: -4px;
-`
+	font-size: 95%;
+	font-weight: 400;
+	text-align: center;
+	margin-top: 0px;
+	margin-bottom: 0px;
+`;
 
 const PostsText = styled.p`
-    font-size: 95%;
-    font-weight: 700;
-    text-align: center;
-`
+	font-size: 95%;
+	font-weight: 700;
+	text-align: center;
+	margin-top: 0px;
+	margin-bottom: 0px;
+`;
 
 const PostsCounterText = styled.p`
-    font-size: 95%;
-    font-weight: 400;
-    text-align: center;
-    margin-top: -4px;
-`
+	font-size: 95%;
+	font-weight: 400;
+	text-align: center;
+	margin-top: 0px;
+	margin-bottom: 0px;
+`;
