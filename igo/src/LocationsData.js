11b1486d68d95ef862ff2c16014a4ebd2499d0a7
Lucas Vicente <yugi.man22@gmail.com>
2019-05-15 01:29:03 +0100

    Images added

--- igo/src/LocationsData.js
+++ igo/src/LocationsData.js
@@ -2,6 +2,18 @@ const LocationsData = [
     {
       id: 1,
       image: "img/Rome.png"
+    },
+    {
+      id: 2,
+      image: "img/Lisboa.png"
+    },
+    {
+      id: 3,
+      image: "img/Porto.png"
+    },
+    {
+      id: 4,
+      image: "img/Barcelona.png"
     }
 ];
   
